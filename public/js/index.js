b49c7badef0b5bb492952ce0d094b47c8bb69294
unknown <aziz.mandasaur@gmail.com>
2019-02-10 17:34:31 +0530

    Add styling to our chat page

--- public/js/index.js
+++ public/js/index.js
@@ -31,11 +31,12 @@ socket.on('disconnect', function () {
 $("#message-form").on("submit", function(e) {
     e.preventDefault();
 
+    var messageTextbox = $("#message");
     socket.emit('createMessage', {
         from: 'User',
-        text: $("#message").val(),
+        text: messageTextbox.val(),
     }, function (data) {
-        console.log('Got it', data);
+        messageTextbox.val('')
     });
 });
 
@@ -44,12 +45,16 @@ locationButton.on("click", function() {
     if(!navigator.geolocation){
         return alert("Geolocation is not supported in your browser");
     } 
+
+    locationButton.attr('disabled', 'disabled').text('Sending location...')
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function () {
+        locationButton.removeAttr('disabled').text('Send location');
         alert("Unable to fetch location");
     });
 });
